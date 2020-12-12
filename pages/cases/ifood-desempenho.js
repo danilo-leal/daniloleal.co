a463d4d19b8f960c3a9f8e80098a50ed5427e630
Danilo Leal <70350062+d-leal@users.noreply.github.com>
2020-12-12 00:52:48 -0300

    creating functions for some pages

---
+++ pages/cases/ifood-desempenho.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function iFoodDesempenho() {
+  return <div></div>;
+}
---
+++ pages/cases/ifood-onboarding.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function iFoodOnboarding() {
+  return <div></div>;
+}
