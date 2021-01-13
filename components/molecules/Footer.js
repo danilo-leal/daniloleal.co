908a31d2573396f118c5250ca45804829f86e4b2
Danilo Leal <daniloleal09@gmail.com>
2021-01-13 10:16:57 -0300

    changing the year on footer

--- components/molecules/Footer.js
+++ components/molecules/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="pt-6 sm:pb-10 pb-8 mt-12 grid grid-flow-row justify-center w-full border-t border-gray-200 dark:border-black">
       <p className="pt-5 pb-5 text-xs font-medium text-center text-gray-700  dark:text-gray-200">
-        danilo leal © 2020
+        danilo leal © 2021
         <br />
         <br /> <span className="opacity-30">•</span>
       </p>
