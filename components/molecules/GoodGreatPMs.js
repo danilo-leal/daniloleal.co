2291ea49d042d42eec05755a21f8206c6976cd8d
danilo leal <daniloleal09@gmail.com>
2021-04-11 22:06:17 -0300

    fixing z-index

--- components/molecules/GoodGreatPMs.js
+++ components/molecules/GoodGreatPMs.js
@@ -4,7 +4,7 @@ export default function GoodVSGreat(props) {
   const { index, good, great } = props;
   return (
     <div className="relative py-4">
-      <div className="absolute h-6 w-6 bg-draplin flex items-center justify-center font-bold text-tiny text-white rounded-full -ml-2 -my-2 z-10">
+      <div className="absolute h-6 w-6 bg-draplin flex items-center justify-center font-bold text-tiny text-white rounded-full -ml-2 -my-2 z-1">
         {index}
       </div>
       <div className="grid sm:grid-cols-2 border border-gray-200 rounded-lg transition-all hover:bg-gray-50 dark:border-gray-700 dark:hover:bg-black opacity-80 hover:opacity-100  divide-y sm:divide-y-0 sm:divide-x dark:divide-gray-700 ">
--- pages/sketch.js
+++ pages/sketch.js
@@ -13,21 +13,18 @@ export default function Sketch() {
         <div className="p-16">
           <button
             className="p-2 border border-gray-300 dark:border-gray-800 rounded-full hover:bg-gray-100 dark:hover:bg-black transition-all"
-            onClick={toggleIsOpen}
-          >
+            onClick={toggleIsOpen}>
             <svg
               className="w-5 h-5 stroke-current dark:text-gray-300 text-gray-600"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+              xmlns="http://www.w3.org/2000/svg">
               <path
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-              ></path>
+                d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
             </svg>
           </button>
           <div
@@ -35,14 +32,17 @@ export default function Sketch() {
               isOpen
                 ? "m-2 border border-gray-300 dark:border-gray-700 shadow-md rounded w-1/3 hover:shadow-lg transition-all flex flex-col space-y-1"
                 : "hidden"
-            }
-          >
+            }>
             <div className="p-4 hover:bg-gray-100 dark:hover:bg-black transition-all">
               <SecondaryLink string="My world" />
             </div>
             <div className="p-4 hover:bg-gray-100 dark:hover:bg-black transition-all">
               <SecondaryLink string="About me" />
             </div>
+          </div>
+          <div
+            className={isOpen ? "p-4 bg-orange-100 rounded w-full" : "hidden"}>
+            Alert aqui.
           </div>
         </div>
       </div>
